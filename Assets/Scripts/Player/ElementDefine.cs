c18b760b69bbcd4cc89b452c38cf9432c03f468f
sinkect <hyomimg27@gmail.com>
2025-09-02 01:20:01 +0900

    머지가 머지 (하하)

---
+++ Assets/Scripts/Player/ElementDefine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class ElementDefine
+{
+    public string name;
+    public int count;
+
+    public ElementDefine(string elementName, int initialCount)
+    {
+        name = elementName;
+        count = initialCount;
+    }
+}
